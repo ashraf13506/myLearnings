2bff73398521b4dea0000597b0581478277dd3dd
ashraf13506 <ashraf13506@gmail.com>
2023-12-21 23:12:24 +0530

    form_group_array

--- Client_Learning/src/app/components/forms/reactive/formgrouparray/formgrouparray.component.ts
+++ Client_Learning/src/app/components/forms/reactive/formgrouparray/formgrouparray.component.ts
@@ -8,6 +8,8 @@ import { FormArray, FormBuilder, FormControl } from '@angular/forms';
   styleUrls: ['./formgrouparray.component.scss']
 })
 export class FormgrouparrayComponent {
+  userName!: string;
+  gender!: string;
 
   constructor(private formBuilder:FormBuilder){}
   patient_form=this.formBuilder.group({
@@ -26,6 +28,12 @@ export class FormgrouparrayComponent {
 
   })
 
+  userdetails_form=this.formBuilder.group({
+    userName:new FormControl(''),
+    gender:new FormControl(''),
+    adduserformArray:this.formBuilder.array([this.formBuilder.control('')]),
+  })
+
 get aliases()
 {
   return this.patient_form.get('aliases') as FormArray
@@ -34,6 +42,22 @@ get aliases()
 addAlias()
 {
   this.aliases.push(this.formBuilder.control(''))
+}
+
+get UserForm()
+{
+return this.userdetails_form.get('adduserformArray') as FormArray
+
+}
+
+addUserForm()
+{
+  this.UserForm.push(this.formBuilder.control(''))
+}
+
+formValues()
+{
+  console.log(this.userdetails_form.value)
 }
 
 }
